844eaaf6261c3fcad9444f56a561315423c5b91d
William Lima Pereira <willguitaradmfar@gmail.com>
2023-11-15 15:04:45 +0000

    update

--- src/dashboard/dash_andamento_carteiras.js
+++ src/dashboard/dash_andamento_carteiras.js
@@ -2,16 +2,63 @@ return async ({
     utils,
     input
 }) => {
-    const total = 421
+    const is_metadata = utils.path(input, 'is_metadata')
+    const pipeline = utils.path(input, 'pipeline', [])
 
-    const count = utils.progress('Meu progresso', 421)
-
-    for (let i = 0; i < total; i++) {
-        count()
-        await new Promise(resolve => setTimeout(resolve, 500))
+    if (is_metadata) {
+        const fields = [{
+            label: 'Carteira',
+            name: 'wallet',
+            typeField: 'String',
+        }, {
+            label: 'Criado em',
+            name: 'created_at',
+            typeField: 'Date',
+        }, {
+            label: 'Variação',
+            name: 'variation',
+            typeField: 'Number',
+        }, {
+            label: 'Valor',
+            name: 'variation_wallet',
+            typeField: 'Number',
+        }, {
+            label: 'Total',
+            name: 'wallet_total',
+            typeField: 'Number',
+        }]
+        return { fields }
     }
 
+    let aggregate = []
+
+    aggregate.push({
+        $lookup: {
+            from: "wallet",
+            localField: "wallet",
+            foreignField: "_id",
+            as: "wallet"
+        }
+    })
+
+    aggregate.push({
+        $unwind: "$wallet"
+    })
+
+    aggregate.push({
+        $project: {
+            _id: 1,
+            wallet: "$wallet.name",
+            created_at: 1,
+            variation: 1,
+            variation_wallet: 1,
+            wallet_total: 1
+        }
+    })
+
     return {
-        foo: 'bar'
+        data: await utils
+            .coll('wallet_target_user_history')
+            .aggregate(aggregate.concat(pipeline))        
     }
 }
